59f8e7379492f2fafb389a221cb7ffab06355257
rp1336 <155965780+rp1336@users.noreply.github.com>
2024-01-08 17:01:14 +0530

    Initial commit

---
+++ code.js
@@ -0,0 +1,31 @@
+let person1= {
+    name:'Bob',
+    age:20,
+    gender:'male'
+}
+let person2= {
+    name:'George',
+    age:42,
+    gender:'male'
+}
+let person3= {
+    name:'Sara',
+    age:42,
+    gender:'female'
+}
+let person4= {
+    name:'Conor',
+    age:40,
+    gender:'male'
+}
+let person5= {
+    name:'Jennifer',
+    age:42,
+    gender:'female'
+}
+
+let arrofperson = [person1,person2,person3,person4,person5];
+
+for(ele of arrofperson){
+    console.log(ele);
+}
